7611783db353980b4e3cbc3e6e8160a42f4f3d80
nelsontky <nelson@ccb.wtf>
2020-08-18 02:17:16 +0800

    Updates

--- assets/tiles/New Updated Tiles/Station4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Station4" tilewidth="32" tileheight="32" tilecount="8" columns="4">
- <image source="Station4.png" width="135" height="64"/>
-</tileset>
---
+++ assets/tiles/Newer Updated Tiles/LOGO.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="LOGO" tilewidth="32" tileheight="32" tilecount="570" columns="30">
+ <image source="LOGO.png" width="960" height="638"/>
+</tileset>
