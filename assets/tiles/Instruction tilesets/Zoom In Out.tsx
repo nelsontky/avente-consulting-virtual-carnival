2fc57140f86e85102a86abcff7393392e3aa51bd
nelsontky <nelson@ccb.wtf>
2020-08-19 03:08:42 +0800

    Updates

---
+++ assets/tiles/Instruction tilesets/Zoom In Out.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Zoom In Out" tilewidth="32" tileheight="32" tilecount="2133" columns="13">
+ <image source="Zoom In Out.png" width="438" height="146"/>
+</tileset>
