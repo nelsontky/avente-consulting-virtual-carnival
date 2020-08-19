4086f9b845ee6ce406e0a734ad0dfd7091c1064e
nelsontky <nelson@ccb.wtf>
2020-08-19 14:10:37 +0800

    Add DB scripts

--- scripts/firebaseToJson.ts
+++ scripts/firebaseToJson.ts
@@ -1,1 +1,62 @@
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
+
+import serviceAccount from "./avente-carnival-firebase-adminsdk-68o8w-4ddb8b6837.json";
+
+import fs from "fs";
+
+const keys: any = serviceAccount;
+const app = admin.initializeApp({
+  credential: admin.credential.cert(keys),
+  databaseURL: "https://avente-carnival.firebaseio.com",
+});
+
+const auth = app.auth();
+const db = app.firestore();
+
+async function getAllUsers(): Promise<{ uid: string; email: string }[]> {
+  const listUsersRes = await auth.listUsers();
+  return listUsersRes.users.map((user) => ({
+    email: user.email,
+    uid: user.uid,
+  }));
+}
+
+async function getAllDocs() {
+  let results = {};
+
+  const snapshot = await db.collection("users").get();
+  snapshot.forEach((doc) => {
+    const docData = doc.data();
+    const stationData = docData.stationData;
+    delete docData.stationData;
+
+    const timeCompleted =
+      docData.timeCompleted == undefined
+        ? "NIL"
+        : docData.timeCompleted.toDate().toString();
+    results[doc.id] = {
+      ...docData,
+      ...stationData,
+      timeCompleted,
+    };
+  });
+
+  return results;
+}
+
+(async () => {
+  let res = [];
+  const users = await getAllUsers();
+  const data = await getAllDocs();
+
+  for (const user of users) {
+    if (data[user.uid] != undefined) {
+      res.push({
+        ...data[user.uid],
+        email: user.email,
+      });
+    }
+  }
+
+  fs.writeFileSync("scripts/output.json", JSON.stringify(res, null, 2));
+})();
